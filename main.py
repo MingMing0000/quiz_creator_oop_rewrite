ff86bfead9fbfffed73a338c4d39f3c308678229
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-05-19 10:13:44 +0800

    show menu

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from quiz_creator import QuizCreator
 
 make_quiz = QuizCreator()
 print('Welcome to Quiz Creator\n')
+
+while True:
+    print('Menu:')
+    print('1. Create a Quiz')
+    print('2. Exit')
+    choice = input('Choose an option: ')
