092925ccb9b43880689c8ec675ded05e9ff2a950
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-05-19 14:36:13 +0800

    remove redundant code

--- quiz_creator.py
+++ quiz_creator.py
@@ -29,7 +29,6 @@ class QuizCreator:
         quiz_name += '.txt'
         print("Saving quiz...")
         with open(quiz_name, 'a') as file:
-
             for quiz in self.quiz_items:
                 file.write(f"Question: {quiz['question']}\n")
                 file.write(f"A) {quiz['choices'][0]}\n")
@@ -37,16 +36,6 @@ class QuizCreator:
                 file.write(f"C) {quiz['choices'][2]}\n")
                 file.write(f"D) {quiz['choices'][3]}\n")
                 file.write(f"Answer: {quiz['answer']}\n")
-
-            for question in self.quiz_items[0]:
-                file.write(f"Question: {question['question']}\n")
-            for choice in self.quiz_items[1]:
-                file.write(f"a) {choice['choices'][0]}\n")
-                file.write(f"b) {choice['choices'][1]}\n")
-                file.write(f"c) {choice['choices'][2]}\n")
-                file.write(f"d) {choice['choices'][3]}\n")
-            for answer in self.quiz_items[2]:
-                file.write(f"Answer: {answer['answer']}\n")
 
                 file.write('\n')
         print(f'\n---Your quiz has been saved as {quiz_name}.---\n')
