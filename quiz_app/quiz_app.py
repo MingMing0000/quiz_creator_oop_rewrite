b159304d134e9bee15a802472c95212139ce2928
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-05-19 22:27:33 +0800

    add a new window to display the quiz

--- quiz_app/quiz_app.py
+++ quiz_app/quiz_app.py
@@ -6,7 +6,7 @@ class QuizApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Quiz App")
-        self.configure(bg="turquoise")
+        self.configure(bg="#86cecb")
         self.geometry("640x360")
         #add welcome text
         self.label = tk.Label(self, text="Welcome to the Quiz App", font="Roboto 20", bg="#86cecb")
@@ -14,3 +14,9 @@ class QuizApp(tk.Tk):
         #add a button to open the quiz file
         self.open_button = tk.Button(self, text="Open Quiz File", font=("Roboto", 15, "white"), bg="#e12885")
         self.open_button.pack(pady=10)
+    #create a new window to display the quiz
+    def the_quiz(self):
+        self.quiz_window = tk.Toplevel(self)
+        self.quiz_window.title("Quiz App")
+        self.quiz_window.geometry("1280x720")
+        self.quiz_window.configure(bg="#86cecb")
