bd8e2dfe11794f162317a661740de8e573e34342
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-05-19 14:28:42 +0800

    add the question, choices, answer to instance variable

--- quiz_creator.py
+++ quiz_creator.py
@@ -3,8 +3,7 @@
 class QuizCreator:
     def __init__(self):
         self.quiz_items = []
-<<<<<<< HEAD
-=======
+
     def ask_question(self):
         self.question = input("Input your question: ")
 
@@ -17,13 +16,12 @@ class QuizCreator:
     
     def ask_answer(self):
         self.answer = input("Input the correct answer from the choices (A, B, C, D): ")
->>>>>>> 00ec30c (access the other dictionaries inside the list)
 
         #append the inputs into a list
         self.quiz_items.append({
-            'question': question,
-            'choices': [choice_a, choice_b, choice_c, choice_d],
-            'answer': answer
+            'question': self.question,
+            'choices': [self.choice_a, self.choice_b, self.choice_c, self.choice_d],
+            'answer': self.answer
         })
     
     def save_quiz(self):
@@ -31,7 +29,7 @@ class QuizCreator:
         quiz_name += '.txt'
         print("Saving quiz...")
         with open(quiz_name, 'a') as file:
-<<<<<<< HEAD
+
             for quiz in self.quiz_items:
                 file.write(f"Question: {quiz['question']}\n")
                 file.write(f"A) {quiz['choices'][0]}\n")
@@ -39,7 +37,7 @@ class QuizCreator:
                 file.write(f"C) {quiz['choices'][2]}\n")
                 file.write(f"D) {quiz['choices'][3]}\n")
                 file.write(f"Answer: {quiz['answer']}\n")
-=======
+
             for question in self.quiz_items[0]:
                 file.write(f"Question: {question['question']}\n")
             for choice in self.quiz_items[1]:
@@ -49,6 +47,6 @@ class QuizCreator:
                 file.write(f"d) {choice['choices'][3]}\n")
             for answer in self.quiz_items[2]:
                 file.write(f"Answer: {answer['answer']}\n")
->>>>>>> 00ec30c (access the other dictionaries inside the list)
+
                 file.write('\n')
         print(f'\n---Your quiz has been saved as {quiz_name}.---\n')
