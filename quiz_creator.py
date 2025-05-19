ec88f5d5f6fdd25e4a17233ec2a064c414c312a1
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-05-19 12:14:05 +0800

    combine methods to a single method for the whole quiz outline(questions,choices,answer)

--- quiz_creator.py
+++ quiz_creator.py
@@ -3,36 +3,36 @@
 class QuizCreator:
     def __init__(self):
         self.quiz_items = []
-
-    def ask_question(self):
-        question = input("Input your question: ")
-        self.quiz_items.append({"question" : question,})
-
-    def ask_choices(self):
-        print("Input the choices")
-        choice_a = input("Input choice A: ")
-        choice_b = input("Input choice B: ")
-        choice_c = input("Input choice C: ")
-        choice_d = input("Input choice D: ")
-        self.quiz_items.append({"choices" : [choice_a, choice_b, choice_c, choice_d]})
     
-    def ask_answer(self):
-        answer = input("Input the correct answer from the choices (A, B, C, D): ")
-        self.quiz_items.append({"answer" : answer})
+    #ask the user for the question, choices, and answer
+    def make_quiz(self):
+        question = input('Input your question: ')
+        print('Input your choices')
+        choice_a = input('Input choice a: ')
+        choice_b = input('Input choice b: ')
+        choice_c = input('Input choice c: ')
+        choice_d = input('Input choice d: ')
+        answer = input('Input the correct answer from the choices(A,B,C,D): '); print()
 
+        #append the inputs into a list
+        self.quiz_items.append({
+            'question': question,
+            'choices': [choice_a, choice_b, choice_c, choice_d],
+            'answer': answer
+        })
+
+    
     def save_quiz(self):
         quiz_name = input('Enter the file name of the quiz: ')
         quiz_name += '.txt'
         print("Saving quiz...")
         with open(quiz_name, 'a') as file:
-            for question in self.quiz_items[0]:
-                file.write(f"Question: {question['question']}\n")
-            for choice in self.quiz_items[1]:
-                file.write(f"a) {choice['choices'][0]}\n")
-                file.write(f"b) {choice['choices'][1]}\n")
-                file.write(f"c) {choice['choices'][2]}\n")
-                file.write(f"d) {choice['choices'][3]}\n")
-            for answer in self.quiz_items[2]:
-                file.write(f"Answer: {answer['answer']}\n")
+            for quiz in self.quiz_items:
+                file.write(f"Question: {quiz['question']}\n")
+                file.write(f"A) {quiz['choices'][0]}\n")
+                file.write(f"B) {quiz['choices'][1]}\n")
+                file.write(f"C) {quiz['choices'][2]}\n")
+                file.write(f"D) {quiz['choices'][3]}\n")
+                file.write(f"Answer: {quiz['answer']}\n")
                 file.write('\n')
         print(f'\n---Your quiz has been saved as {quiz_name}.---\n')
