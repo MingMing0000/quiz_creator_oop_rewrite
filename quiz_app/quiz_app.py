2f885486d00758e2c80661610576a777319b0364
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-05-19 19:42:03 +0800

    add window config

--- quiz_app/quiz_app.py
+++ quiz_app/quiz_app.py
@@ -4,4 +4,7 @@ import tkinter as tk
 # Create the class QuizApp
 class QuizApp(tk.Tk):
     def __init__(self):
-        pass
+        super().__init__()
+        self.title("Quiz App")
+        self.configure(bg="turquoise")
+        self.geometry("640x360")
